39873b838a78df29f242fb72a149576b867e1b0a
Haider Fahim <haiderfahim.p@gmail.com>
2025-06-05 01:24:24 +0100

    update the code

--- Tarefa 11 -5417-HAIDERFAHIM/webappmotostop1/webappmotostop1/obj/Debug/net8.0/webappmotostop1.AssemblyInfo.cs
+++ Tarefa 11 -5417-HAIDERFAHIM/webappmotostop1/webappmotostop1/obj/Debug/net8.0/webappmotostop1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappmotostop1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d67d0cf32beb6a7fd6addf7dd8d6559f070053")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappmotostop1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappmotostop1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
